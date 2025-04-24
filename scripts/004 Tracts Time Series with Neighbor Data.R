33419475c27e3e85e07e06fd4e8ec8b14d8aa929
kabirakabira <kabir.jain1999@gmail.com>
2025-04-24 14:48:58 -0500

    Fixing Neighbor computation

--- scripts/004 Tracts Time Series with Neighbor Data.R
+++ scripts/004 Tracts Time Series with Neighbor Data.R
@@ -23,22 +23,27 @@ tract.timeseries <- read.csv("data/processed/Tracts_TimeSeries.csv")
 ### tract neighbor groups table
 tract.neighbors <- read.csv("data/processed/Tracts_NeighborGroups.csv")
 
+## Join time series data to neighbor data
+tract.neighbors <- tract.neighbors %>% 
+  left_join(tract.timeseries %>% select(-Is_OZ),
+            by = "GEOID")
+
 ## Summarize Neighbor Data
 tract.neighbors <- tract.neighbors %>%
   mutate(Count = 1) %>%
-  group_by(Parent_GEOID) %>%
+  group_by(Parent_GEOID, Year) %>%
   summarize(
     Total_Neighbors = sum(Count) - 1,
     OZ_Neighbors = sum(Is_OZ), ## This overcounts by 1 where GEOID == Parent_GEOID and Is_OZ == 1
+    N_Pov_Below100 = mean(Pov_Below100, na.rm = T),
+    N_Pov_Below150 = mean(Pov_Below150, na.rm = T),
+    N_MHHI = mean(MHHI, na.rm = T)
+  )
+
+## Join to time series data
+tract.timeseries <- tract.timeseries %>%
+  left_join(tract.neighbors, by = c("GEOID" = "Parent_GEOID", "Year")) %>%
+  mutate(
+    OZ_Neighbors = ifelse(Is_OZ == 1, OZ_Neighbors - 1, OZ_Neighbors) ## Account for the overcount
   )
   
-## Join to Tract Time Series Data and account for the previously mentioned overcount
-tract.timeseries <- tract.timeseries %>%
-  left_join(tract.neighbors,
-            by = c("GEOID" = "Parent_GEOID")) %>%
-  mutate(OZ_Neighbors = ifelse(Is_OZ == 1, OZ_Neighbors - 1, OZ_Neighbors))
-
-## Write out updated tract time series
-write.csv(tract.timeseries,
-          "data/processed/Tracts_TimeSeries_WithNeighborData.csv",
-          row.names = F)
